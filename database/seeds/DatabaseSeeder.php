e04cf7fec37ebc4826bfd00edc80f062ddd15b9b
Christopher Pecoraro <chrispecoraro@gmail.com>
2017-11-30 15:36:53 +0100

    Answer User Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
-         $this->call(UsersAnswersTableSeeder::class);
+         $this->call(UserAnswerSeeder::class);
+        $this->call(ElephantsTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,18 +12,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // option 1: setting single properties
-        $user = new User;
-        $user->name = 'Indiana Jones';
-        $user->email = 'indiana.jones@gmail.com';
-        $user->password = Hash::make('Dk324c#$34-');
-        $user->save();
+//        // option 1: setting single properties
+//        $user = new User();
+//        $user->name = 'Indiana Jones';
+//        $user->email = 'indiana.jones@gmail.com';
+//        $user->password = Hash::make('Dk324c#$34-');
+////        $user->api_token = str_random(60);
+//        $user->save();
+//
+//        // option 1: setting single properties
+//        $user = new User();
+//        $user->name = 'Christopher Pecoraro';
+//        $user->email = 'chrispecoraro@gmail.com';
+//        $user->password = Hash::make('password');
+//        $user->api_token = str_random(60);
+//        $user->save();
 
-        // option 2: setting an array of attributes
-        User::create(['name'=>'Clark Kent',
-                      'email'=>'clark.kent@gmail.com',
-                      'password'=>bcrypt('a99sck@ljc')
-                    ]);
+
+//        // option 2: setting an array of attributes
+//        User::create(['name'      => 'Clark Kent',
+//                      'email'     => 'clark.kent@gmail.com',
+//                      'password'  => bcrypt('a99sck@ljc'),
+////                      'api_token' => str_random(60)
+//                    ]);
 
         // option 3: using faker -- creates 10 users
         factory(User::class, 10)->create();
